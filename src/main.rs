08e24d0359f79fa752f6559a084f6db40b6e4d3f
BrianKopp <briankopp.usa@gmail.com>
2019-06-15 22:52:57 -0600

    Remove print args. Import process and io. Check for error and print message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::io;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
-    let content = fs::read_to_string(&args[1])
-        .expect("should have been able to read file");
-    println!("{}", content);
+    let file_name = &args[1];
+    let file_content_result = fs::read_to_string(file_name);
+    if let Err(err) = file_content_result {
+        match err.kind() {
+            io::ErrorKind::NotFound => println!("file '{}' not found", file_name),
+            _ => println!("unexpected error occurred: {}", err),
+        }
+        process::exit(1)
+    }
+    let contents = file_content_result.unwrap();
+    println!("{}", contents);
 }
