6a37b7ad7330637475f4ac93cdc6dbffb70f87db
BrianKopp <briankopp.usa@gmail.com>
2019-06-15 22:45:27 -0600

    read and print out file contents

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    let content = fs::read_to_string(&args[1])
+        .expect("should have been able to read file");
+    println!("{}", content);
 }
